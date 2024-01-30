5d5df4adb684344f356494b411278d9a020fb77c
root <root@ip-172-31-7-140.ap-south-1.compute.internal>
2024-01-30 15:53:14 +0000

    okey

--- appmesh-canary-deployment.sh
+++ appmesh-canary-deployment.sh
@@ -33,25 +33,33 @@ sleep 60  # Sleep for 5 minutes
 
 
 ##
+
 # Check metrics (example: error rate)
 log "Checking metrics (error rate)"
 error_rate=$(aws cloudwatch get-metric-data --region $AWS_REGION \
   --start-time $(date -u +%Y-%m-%dT%H:%M:%SZ --date '-5 minutes') \
   --end-time $(date -u +%Y-%m-%dT%H:%M:%SZ) \
-  --metric-data-queries "[{\"id\":\"m1\",\"metricStat\":{\"metric\":{\"dimensions\":[{\"name\":\"VirtualService\",\"value\":\"$VIRTUAL_SERVICE_NAME\"},{\"name\":\"Mesh\",\"value\":\"$APPMESH_NAME\"},{\"name\":\"VirtualRouter\",\"value\":\"$VIRTUAL_ROUTER_NAME\"}],\"metricName\":\"4xxError\",\"namespace\":\"AWS/AppMesh\"},\"period\":300,\"stat\":\"Sum\",\"unit\":\"Count\"},\"returnData\":true}]" \
-  --scan-by "TimestampDescending" \
+  --metric-data-queries '[
+    {
+      "Id": "m1",
+      "MetricStat": {
+        "Metric": {
+          "Dimensions": [
+            {"Name": "VirtualService", "Value": "'$VIRTUAL_SERVICE_NAME'"},
+            {"Name": "Mesh", "Value": "'$APPMESH_NAME'"},
+            {"Name": "VirtualRouter", "Value": "'$VIRTUAL_ROUTER_NAME'"}
+          ],
+          "MetricName": "4xxError",
+          "Namespace": "AWS/AppMesh"
+        },
+        "Period": 300,
+        "Stat": "Sum",
+        "Unit": "Count"
+      }
+    }
+  ]' \
   --output json \
   --query 'MetricDataResults[0].Values[0]')
-
-
-
-
-
-
-
-
-
-
 
 
 
