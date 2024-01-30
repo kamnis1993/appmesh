fe5ce28e964ce2f5ce6587c4b43d8f44881e3315
root <root@ip-172-31-7-140.ap-south-1.compute.internal>
2024-01-30 10:10:00 +0000

    addn

--- ecs-deployment.sh
+++ ecs-deployment.sh
@@ -7,17 +7,21 @@ SECRET_KEY=igmM8hV2S5kYJYlGXe6lIv5f/DkMdroiQJf3PpdH
 COMMIT_HASH=default
 ECS_SERVICE_ARN=arn:aws:ecs:ap-northeast-1:381492111475:cluster/example
 
-# Your ECS deployment commands here
+# Define an array of task definitions
+TASK_DEFINITIONS=("examplegw:1" "example2:1" "example1:1")
 
-# Example ECS Update Service Command
-aws ecs update-service \
-  --region $REGION \
-  --cluster example \
-  --service $ECS_SERVICE_ARN \
-  --task-definition examplegw:1
+# Iterate through task definitions and update ECS service
+for TASK_DEF in "${TASK_DEFINITIONS[@]}"; do
+  aws ecs update-service \
+    --region $REGION \
+    --cluster example \
+    --service $ECS_SERVICE_ARN \
+    --task-definition $TASK_DEF
 
-# Tag ECS tasks with the commit hash
-#aws ecs tag-resource \
-#  --region $REGION \
-#  --resource-arn $ECS_SERVICE_ARN \
-#  --tags Key=Name,Value=$COMMIT_HASH
+  # Tag ECS tasks with the commit hash
+  aws ecs tag-resource \
+    --region $REGION \
+    --resource-arn $ECS_SERVICE_ARN \
+    --tags Key=CommitHash,Value=$COMMIT_HASH
+done
+
