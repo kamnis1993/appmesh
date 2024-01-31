386e67353b0745e1744c2016bc90a0e3e5f1a495
root <root@ip-172-31-9-190.ap-northeast-1.compute.internal>
2024-01-31 10:25:15 +0000

    asif

--- ecs-deployment.sh
+++ ecs-deployment.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 REGION=ap-northeast-1
-ACCOUNT_ID=381492111475
+ACCOUNT_ID=967866550848
 ACCESS_KEY=AKIAVRUVTVBZ5ZX2AMFX
 SECRET_KEY=igmM8hV2S5kYJYlGXe6lIv5f/DkMdroiQJf3PpdH
 COMMIT_HASH=default
-ECS_SERVICE_ARN=arn:aws:ecs:ap-northeast-1:381492111475:cluster/example
+ECS_SERVICE_ARN=arn:aws:ecs:ap-northeast-1:967866550848:cluster/example
 
 # Define an array of task definitions
 TASK_DEFINITIONS=("examplegw:1" "example2:1" "example1:1")
